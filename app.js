370e9041128d2b131c166acdad244f3e12841b01
Dee Davis <threedaycleanse@gmail.com>
2017-04-07 19:15:14 -0400

    Updated DB

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
+
 pry = require('pryjs');
 var express = require('express');
 var mongoose = require('mongoose');
--- db/seeds.js
+++ db/seeds.js
@@ -36,3 +36,18 @@ dee.save(function(err) {
 
   console.log('dee created!');
 });
+// Connect to database
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
